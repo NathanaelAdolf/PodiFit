6163e6d02e27f6a1208f82fb4c844048aca30323
NathanaelAdolf <55471987+NathanaelAdolf@users.noreply.github.com>
2020-10-23 10:49:51 +0700

    update

--- PodiFit/PodiFit/HomeProfile/HomeProfileViewController.swift
+++ PodiFit/PodiFit/HomeProfile/HomeProfileViewController.swift
@@ -8,38 +8,162 @@
 
 import UIKit
 
-class HomeProfileViewController: UIViewController {
+class HomeProfileViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
+    
+    var completedData = [CompletedPlanModel]()
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        //return 5 nanti bikin jadi 5 setelah semua cellnya jadi
+        
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        if section == 0 {
+            return 1
+        }
+        else if section == 1{
+            return 1
+        }
+        else if section == 2
+        {
+            return 1
+        }
+        else
+        {
+            return 2
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.section == 0
+        {
+            return 110
+        }
+        else if indexPath.section == 1
+        {
+            return 140
+        }
+        else if indexPath.section == 2
+        {
+            return 152
+        }
+        else if indexPath.section == 3
+        {
+            return 80
+        }
+        else
+        {
+            return 100
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == 0
+        {
+             let cell = tableView.dequeueReusableCell(withIdentifier: "imagePersonCell", for: indexPath) as! imagePersonTableViewCell
+            
+            cell.persomImage.image = UIImage(systemName: "folder")
+            
+            return cell
+            
+        }
+        else if indexPath.section == 1
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "userDataCell", for: indexPath) as! userDataTableViewCell
+            
+            //nanti ganti dengan input user
+            cell.numberOfActivePlansLabel.text = "0"
+            cell.numberWeightLabel.text = "0"
+            cell.heightLabel.text = "0"
+            
+            return cell
+            
+            
+        }
+        else if indexPath.section == 2
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "completedPlanCell", for: indexPath) as! CompletedPlanTableViewCell
+            
+            cell.loadCollectionView(data: completedData)
+            
+            return cell
+        }
+        else
+        {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "completedPlanCell", for: indexPath) as! CompletedPlanTableViewCell
+            
+            return cell
+        }
+        
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        if section == 0
+        {
+            return ""
+        }
+        else if section == 1
+        {
+            return ""
+        }
+        else if section == 2 {
+            return "Completed Plan"
+        }
+        else if section == 3
+        {
+            return "Reminder"
+        }
+        else if section == 4
+        {
+            return "badges"
+        }
+        else {
+            return ""
+        }
+    }
+    
+    /*func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+           
+             let headerView = UIView()
+             headerView.backgroundColor = UIColor.white
+
+             let sectionLabel = UILabel(frame: CGRect(x: 8, y: 28, width:
+             tableView.bounds.size.width, height: tableView.bounds.size.height))
+             sectionLabel.font = UIFont(name: "Helvetica", size: 20)
+             sectionLabel.font = UIFont.boldSystemFont(ofSize: 20)
+             sectionLabel.textColor = UIColor.black
+           sectionLabel.text = "Cook"
+             sectionLabel.sizeToFit()
+             headerView.addSubview(sectionLabel)
+
+             return headerView
+       }*/
+    
 
     @IBOutlet weak var completeRemindBadgeTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
-      //  completeRemindBadgeTableView.delegate = self
-       // completeRemindBadgeTableView.dataSource = self
+        completeRemindBadgeTableView.delegate = self
+        completeRemindBadgeTableView.dataSource = self
+        
+        completeRemindBadgeTableView.separatorStyle = .none
+        
+        //data dummy buat completedData
+        self.completedData =
+            [
+                CompletedPlanModel(titleMovement: "Leg Plan", level: "Beginner", period: 4)
+                ,CompletedPlanModel(titleMovement: "Advance leg plan", level: "Intermediate", period: 6)
+            ]
+        
+        
     }
     
-
-  
-    
-
 }
-
-/*extension HomeProfileViewController: UITableViewDelegate,UITableViewDataSource
-{
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
-        
-    }
-    
-    
-}*/
 
 
---
+++ PodiFit/PodiFit/HomeProfile/imagePersonTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  imagePersonTableViewCell.swift
+//  PodiFit
+//
+//  Created by Nathanael Adolf Sukiman on 23/10/20.
+//  Copyright © 2020 Nathanael Adolf Sukiman. All rights reserved.
+//
+
+import UIKit
+
+class imagePersonTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var persomImage: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
