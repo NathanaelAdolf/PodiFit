8b861181529bc06604102ab321015f72b1175775
Hafizul Ihsan Fadli <hafizulihsanfadli@gmail.com>
2020-11-02 12:39:00 +0700

    Add UI Assesment dan HomePlan

---
+++ PodiFit/PodiFit/Assesment/AssesmentViewController.swift
@@ -0,0 +1,18 @@
+//
+//  Assesment.swift
+//  PodiFit
+//
+//  Created by Nathanael Adolf Sukiman on 21/10/20.
+//  Copyright © 2020 Nathanael Adolf Sukiman. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+
+class AssesmentViewController: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
