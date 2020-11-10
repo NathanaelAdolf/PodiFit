55e7790f8bd8c5ff06dee556a8677550de149260
Hafizul Ihsan Fadli <hafizulihsanfadli@gmail.com>
2020-11-10 10:35:10 +0700

    ChoseePlan update

--- PodiFit/PodiFit/ChoosePlan/PlanOverview/PlanListOverviewTableViewCell.swift
+++ PodiFit/PodiFit/ChoosePlan/PlanOverview/PlanListOverviewTableViewCell.swift
@@ -19,7 +19,6 @@ class PlanListOverviewTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
-        cardBackground.layer.cornerRadius = 10
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
