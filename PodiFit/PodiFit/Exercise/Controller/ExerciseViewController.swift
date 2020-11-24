6d1ba15f6c33aa2488a8ab8db4747255eedcdbea
NathanaelAdolf <55471987+NathanaelAdolf@users.noreply.github.com>
2020-11-24 12:49:51 +0700

    initial test gabung file vicky udah jalan

--- PodiFit/PodiFit/Exercise/Controller/ExerciseViewController.swift
+++ PodiFit/PodiFit/Exercise/Controller/ExerciseViewController.swift
@@ -39,13 +39,26 @@ class ExerciseViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+//        AppUtility.lockOrientation(.landscape)
         circularProgressView.trackClr = UIColor(red: 95/255, green: 104/255, blue: 71/255, alpha: 100)
         circularProgressView.progressClr = UIColor.init(red: 228/255, green: 246/255, blue: 80/255, alpha: 100)
-        
+        print(". kok ngga keluar \(finishExercise)")
         setupInit(data: finishExercise)
         self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(allSummaryTime), userInfo: nil, repeats: true)
        
+    }
+    
+
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        AppUtility.lockOrientation(.portrait)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        AppUtility.lockOrientation(.all)
+        
     }
     
     public func setupInit(data : Int) {
@@ -62,7 +75,10 @@ class ExerciseViewController: UIViewController {
         }
             
         if data != 0 {
+            print("ini data \(data)")
+//            setupInit(data: 0)
             self.tempExerciseDetailPrevious = planModelHelpers.fetchExerciseDetail(idExercise: idExercises![data-1])
+            
         }
         
         // ambil warning datanya
@@ -82,9 +98,14 @@ class ExerciseViewController: UIViewController {
         
     }
     
+    @IBAction func unwindSegueFromExit(_ sender: UIStoryboardSegue) {
+        
+    }
+    
     
     
     @IBAction func informationExercise(_ sender: Any) {
+        AppUtility.lockOrientation(.portrait)
         self.performSegue(withIdentifier: "toInformationExercise", sender: nil)
         
     }
@@ -103,8 +124,11 @@ class ExerciseViewController: UIViewController {
     }
     
     @IBAction func previous(_ sender: Any) {
+        
         if finishExercise == (countChosenExercise - 1) {
             exerciseView.lastExercise()
+        } else if finishExercise == 0 {
+            finishExercise = 0
         } else {
             if ((isVideo % 2) != 1) {
                 exerciseView.restView(dataExercise : tempExerciseDetailPrevious, number: (finishExercise + 1), totalExercise: countChosenExercise)
@@ -112,7 +136,11 @@ class ExerciseViewController: UIViewController {
                 self.timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(countDownTimer), userInfo: nil, repeats: true)
                 isVideo += 1
             }
+            
+            
             finishExercise -= 1
+            
+            
             print("ini next \(finishExercise)")
         }
     }
